802b8fcef4e90a324abc808f9560818220c8514c
Aleksandr Razumov <ar@cydev.ru>
2016-04-30 14:26:13 +0300

    ci: update test

--- ci/test.sh
+++ ci/test.sh
@@ -2,8 +2,14 @@
 
 set -e -u -x
 
-export GOPATH=$PWD/cydev-stun
+export GOPATH=$PWD
+export PATH=$GOPATH/bin:$PATH
+mkdir -p src/github.com/cydev
+cp -R cydev-stun src/github.com/cydev/stun
 
-cd cydev-stun
-go get -t
-go test -race
+pushd ${GOPATH}/src/github.com/cydev/stun
+    go get -t
+    go build
+    TEST_EXTERNAL=1 go test
+popd
+
