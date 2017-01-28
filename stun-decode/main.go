3816e192e0e4c4242586be1e83bb610027b21421
Aleksandr Razumov <ar@cydev.ru>
2017-01-28 04:21:09 +0300

    stun: add stun-decode utility to decode base64 dumps

---
+++ stun-decode/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ernado/stun"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "stun-decode")
+		fmt.Fprintln(os.Stderr, "stun-decode AAEAHCESpEJML0JTQWsyVXkwcmGALwAWaHR0cDovL2xvY2FsaG9zdDozMDAwLwAA")
+		fmt.Fprintln(os.Stderr, "First argument must be a base64.StdEncoding-encoded message")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	data, err := base64.StdEncoding.DecodeString(flag.Arg(0))
+	if err != nil {
+		log.Fatalln("Unable to decode bas64 value:", err)
+	}
+	m := stun.AcquireMessage()
+	if _, err = m.ReadBytes(data); err != nil {
+		log.Fatalln("Unable to decode message:", err)
+	}
+	fmt.Println(m)
+}
