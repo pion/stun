2c52339fa18f90b991bc5a1a01b5ed6fced1791d
Aleksandr Razumov <ar@cydev.ru>
2017-02-12 19:01:03 +0300

    a:username: implement

---
+++ username.go
@@ -0,0 +1,44 @@
+package stun
+
+import "errors"
+
+// NewUsername returns *Username with provided value.
+func NewUsername(username string) *Username {
+	return &Username{
+		Raw: []byte(username),
+	}
+}
+
+// Username represents USERNAME attribute.
+//
+// https://tools.ietf.org/html/rfc5389#section-15.3
+type Username struct {
+	Raw []byte
+}
+
+func (u Username) String() string {
+	return string(u.Raw)
+}
+
+const maxUsernameB = 513
+
+// ErrUsernameTooBig means that USERNAME value is bigger that 513 bytes.
+var ErrUsernameTooBig = errors.New("USERNAME value bigger than 513 bytes")
+
+// Username
+func (u *Username) AddTo(m *Message) error {
+	if len(u.Raw) > maxUsernameB {
+		return ErrUsernameTooBig
+	}
+	m.Add(AttrUsername, u.Raw)
+	return nil
+}
+
+func (u *Username) GetFrom(m *Message) error {
+	v, err := m.Get(AttrUsername)
+	if err != nil {
+		return err
+	}
+	u.Raw = v
+	return nil
+}
---
+++ username_test.go
@@ -0,0 +1,45 @@
+package stun
+
+import (
+	"testing"
+)
+
+func TestUsername(t *testing.T) {
+	username := "username"
+	u := NewUsername(username)
+	m := new(Message)
+	m.WriteHeader()
+	t.Run("Bad length", func(t *testing.T) {
+		badU := &Username{Raw: make([]byte, 600)}
+		if err := badU.AddTo(m); err != ErrUsernameTooBig {
+			t.Errorf("expected %s, got %v", ErrUsernameTooBig, err)
+		}
+	})
+	t.Run("AddTo", func(t *testing.T) {
+		if err := u.AddTo(m); err != nil {
+			t.Error("errored:", err)
+		}
+		t.Run("GetFrom", func(t *testing.T) {
+			got := new(Username)
+			if err := got.GetFrom(m); err != nil {
+				t.Error("errored:", err)
+			}
+			if got.String() != username {
+				t.Errorf("expedted: %s, got: %s", username, got)
+			}
+		})
+	})
+	t.Run("No allocations", func(t *testing.T) {
+		m := new(Message)
+		m.WriteHeader()
+		u := NewUsername("username")
+		if allocs := testing.AllocsPerRun(10, func() {
+			if err := u.AddTo(m); err != nil {
+				t.Error(err)
+			}
+			m.Reset()
+		}); allocs > 0 {
+			t.Errorf("got %f allocations, zero expected", allocs)
+		}
+	})
+}
