2f1c33711b905f345ed51b173077ebe8726970b8
Aleksandr Razumov <ar@cydev.ru>
2017-02-12 19:02:04 +0300

    docs: remove "Definitions" not related to package

--- message.go
+++ message.go
@@ -1,23 +1,4 @@
 // Package stun implements Session Traversal Utilities for NAT (STUN) RFC 5389.
-//
-// Definitions
-//
-// STUN Agent: A STUN agent is an entity that implements the STUN
-// protocol. The entity can be either a STUN client or a STUN
-// server.
-//
-// STUN Client: A STUN client is an entity that sends STUN requests and
-// receives STUN responses. A STUN client can also send indications.
-// In this specification, the terms STUN client and client are
-// synonymous.
-//
-// STUN Server: A STUN server is an entity that receives STUN requests
-// and sends STUN responses. A STUN server can also send
-// indications. In this specification, the terms STUN server and
-// server are synonymous.
-//
-// Transport Address: The combination of an IP address and Port number
-// (such as a UDP or TCP Port number).
 package stun
 
 import (
