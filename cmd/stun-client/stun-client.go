dff5ec6d8e72bcce7b002f767cc41d2f96d3d47e
Aleksandr Razumov <ar@cydev.ru>
2017-07-19 01:20:13 +0200

    client: update stun-client command

--- cmd/stun-client/stun-client.go
+++ cmd/stun-client/stun-client.go
@@ -5,43 +5,41 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
+	"time"
 
 	"github.com/ernado/stun"
 )
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, os.Args[0], "stun.l.google.com:19302")
+	}
 	flag.Parse()
 	addr := flag.Arg(0)
 	if len(addr) == 0 {
-		fmt.Fprintln(os.Stderr, "no uri specified")
+		fmt.Fprintln(os.Stderr, "no address specified")
+		flag.Usage()
 		os.Exit(2)
 	}
-
-	c, err := stun.Dial(addr)
+	c, err := stun.Dial("udp", addr)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		if err := c.ReadUntilClosed(); err != nil {
-			log.Fatalln("read until closed loop:", err)
+	deadline := time.Now().Add(time.Second * 5)
+	if err := c.Do(stun.MustBuild(stun.TransactionID, stun.BindingRequest), deadline, func(res stun.AgentEvent) {
+		if res.Error != nil {
+			log.Fatalln(err)
 		}
-		wg.Done()
-	}()
-
-	if err := c.Do(stun.MustBuild(stun.TransactionID, stun.BindingRequest), func(res *stun.Message) error {
 		var xorAddr stun.XORMappedAddress
-		if err := xorAddr.GetFrom(res); err != nil {
-			return err
+		if err := xorAddr.GetFrom(res.Message); err != nil {
+			log.Fatalln(err)
 		}
 		fmt.Println(xorAddr)
-		return nil
 	}); err != nil {
 		log.Fatal("do:", err)
 	}
-	c.Close()
-	wg.Wait()
+	if err := c.Close(); err != nil {
+		log.Fatalln(err)
+	}
 }
