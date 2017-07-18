626e9df563f88503b9215b78885240b1f6388308
Aleksandr Razumov <ar@cydev.ru>
2017-07-19 00:01:48 +0200

    client: update integration test

--- integration-test/main.go
+++ integration-test/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
 	"time"
 
 	"github.com/ernado/stun"
@@ -25,31 +24,32 @@ func main() {
 		time.Sleep(time.Millisecond * 300 * time.Duration(i))
 	}
 	if err != nil {
-		log.Println("too many attempts to resolve:", err)
+		log.Fatalln("too many attempts to resolve:", err)
 	}
 
-	client := new(stun.Client)
 	fmt.Println("DIALING", addr)
-	if err = client.Dial(addr); err != nil {
-		log.Fatalln("failed to client.Dial:", err)
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		log.Fatalln("failed to dial:", err)
 	}
-
-	wg := new(sync.WaitGroup)
-	wg.Add(1)
-	go func() {
-		if err := client.ReadUntilClosed(); err != nil {
-			log.Fatalln("read until closed loop:", err)
-		}
-		wg.Done()
-	}()
-
-	laddr := client.LocalAddr()
+	client := stun.NewClient(stun.ClientOptions{
+		Connection: conn,
+	})
+	laddr := conn.LocalAddr()
 	fmt.Println("LISTEN ON", laddr)
+
 	request, err := stun.Build(stun.BindingRequest, stun.TransactionID)
 	if err != nil {
 		log.Fatalln("failed to build:", err)
 	}
-	if err := client.Do(request, func(response *stun.Message) error {
+	timeout := time.Second
+	deadline := time.Now().Add(timeout)
+	processed := make(chan struct{})
+	if err := client.Do(request, deadline, func(event stun.AgentEvent) {
+		if event.Error != nil {
+			log.Fatalln("got event with error:", event.Error)
+		}
+		response := event.Message
 		if response.Type != stun.BindingSuccess {
 			log.Fatalln("bad message", response)
 		}
@@ -61,10 +61,17 @@ func main() {
 			log.Fatalln(laddr, "!=", xorMapped)
 		}
 		fmt.Println("OK", response, "GOT", xorMapped)
-		return nil
+		processed <- struct{}{}
 	}); err != nil {
 		log.Fatalln("failed to Do:", err)
 	}
-	client.Close()
-	wg.Wait()
+	select {
+	case <-processed:
+		log.Println("PROCESSED")
+	case <-time.After(timeout * 3):
+		log.Fatalln("CALLBACK IS NOT CALLED")
+	}
+	if err := client.Close(); err != nil {
+		log.Fatalln("failed to close client:", err)
+	}
 }
