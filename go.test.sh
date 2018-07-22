43700cb710ee5cf5f9d4640c072d868ba7b5a581
Aleksandr Razumov <ar@cydev.ru>
2018-07-22 07:55:52 +0300

    ci: test all subpackages, use codecov

---
+++ go.test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+touch coverage.txt
+
+# test fuzz inputs
+go test -tags gofuzz -run TestFuzz -v .
+
+# quick-test without -race
+go test ./...
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic "$d"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
