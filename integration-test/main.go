2b28b38211ebc584842d4aa4af8b9ca02e392fa2
Aleksandr Razumov <ar@cydev.ru>
2018-07-22 07:47:46 +0300

    e2e: fix lint warning

--- integration-test/main.go
+++ integration-test/main.go
@@ -47,7 +47,7 @@ func main() {
 	}
 	timeout := time.Second
 	deadline := time.Now().Add(timeout)
-	if err := client.Do(request, deadline, func(event stun.Event) {
+	if err = client.Do(request, deadline, func(event stun.Event) {
 		if event.Error != nil {
 			log.Fatalln("got event with error:", event.Error)
 		}
