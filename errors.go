b05975a399f1097c9289a41ca888af03fa003289
Aleksandr Razumov <ar@cydev.ru>
2018-08-09 02:56:56 +0300

    errors: remove ErrAttributeSizeInvalid

--- errors.go
+++ errors.go
@@ -55,13 +55,8 @@ func newAttrDecodeErr(children, message string) *DecodeErr {
 	return newDecodeErr("attribute", children, message)
 }
 
-// ErrAttrSizeInvalid means that decoded attribute size is invalid.
-//
-// DEPRECATED: use ErrAttributeSizeInvalid.
-var ErrAttrSizeInvalid = errors.New("attribute size is invalid")
-
 // ErrAttributeSizeInvalid means that decoded attribute size is invalid.
-var ErrAttributeSizeInvalid = ErrAttrSizeInvalid
+var ErrAttributeSizeInvalid = errors.New("attribute size is invalid")
 
 // ErrAttributeSizeOverflow means that decoded attribute size is too big.
 var ErrAttributeSizeOverflow = errors.New("attribute size overflow")
