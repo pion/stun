d2138f5f938a2adb0020d5511b986a11112dd4b4
Steffen Vogel <post@steffenvogel.de>
2023-01-23 12:26:52 +0100

    Replace go-fuzz by Go's built-in fuzz tooling
    As introduced by Go 1.18
    
    Co-authored-by: Atsushi Watanabe <atsushi.w@ieee.org>

--- fuzz_test.go
+++ fuzz_test.go
@@ -1,96 +1,151 @@
-// +build gofuzz
-
 package stun
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
+	"encoding/binary"
+	"errors"
 	"testing"
 )
 
-func TestMessageType_FuzzerCrash1(t *testing.T) {
-	input := []byte("\x9c\xbe\x03")
-	FuzzType(input)
+func FuzzMessage(f *testing.F) {
+	msg1 := New()
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		msg1.Reset()
+
+		// Fuzzer does not know about cookies
+		if len(data) >= 8 {
+			binary.BigEndian.PutUint32(data[4:8], magicCookie)
+		}
+
+		// Trying to read data as message
+		if _, err := msg1.Write(data); err != nil {
+			return // We expect invalid messages to fail here
+		}
+
+		msg2 := New()
+		if _, err := msg2.Write(msg1.Raw); err != nil {
+			t.Fatalf("Failed to write: %s", err)
+		}
+
+		if msg2.TransactionID != msg1.TransactionID {
+			t.Fatal("Transaction ID mismatch")
+		}
+
+		if msg2.Type != msg1.Type {
+			t.Fatal("Type mismatch")
+		}
+
+		if len(msg2.Attributes) != len(msg1.Attributes) {
+			t.Fatal("Attributes length mismatch")
+		}
+	})
 }
 
-func TestMessageCrash2(t *testing.T) {
-	input := []byte("00\x00\x000000000000000000")
-	FuzzMessage(input)
+func FuzzType(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data uint16) {
+		v := data & 0x1fff // First 3 bits are empty
+
+		t1 := MessageType{}
+		t1.ReadValue(v)
+		v2 := t1.Value()
+		if v != v2 {
+			t.Fatal("v != v2")
+		}
+
+		t2 := MessageType{}
+		t2.ReadValue(v2)
+		if t2 != t1 {
+			t.Fatal("t2 != t1")
+		}
+	})
 }
 
-func corpus(t *testing.T, function, typ string) [][]byte {
-	var data [][]byte
-	p := filepath.Join("fuzz", function, typ)
-	f, err := os.Open(p)
-	if err != nil {
-		if os.IsNotExist(err) {
-			t.Skip("does not exist")
+func FuzzSetters(f *testing.F) {
+	f.Fuzz(func(t *testing.T, firstByte byte, value []byte) {
+		var (
+			m1 = &Message{
+				Raw: make([]byte, 0, 2048),
+			}
+			m2 = &Message{
+				Raw: make([]byte, 0, 2048),
+			}
+			m3 = &Message{
+				Raw: make([]byte, 0, 2048),
+			}
+		)
+
+		attrs := attributes{
+			{new(Realm), AttrRealm},
+			{new(XORMappedAddress), AttrXORMappedAddress},
+			{new(Nonce), AttrNonce},
+			{new(Software), AttrSoftware},
+			{new(AlternateServer), AttrAlternateServer},
+			{new(ErrorCodeAttribute), AttrErrorCode},
+			{new(UnknownAttributes), AttrUnknownAttributes},
+			{new(Username), AttrUsername},
+			{new(MappedAddress), AttrMappedAddress},
+			{new(Realm), AttrRealm},
 		}
-		t.Fatal(err)
-	}
-	list, err := f.Readdir(-1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for _, d := range list {
-		if strings.Contains(d.Name(), ".") {
-			// Skipping non-raw files.
-			continue
+		attr := attrs.pick(firstByte)
+
+		m1.WriteHeader()
+		m1.Add(attr.t, value)
+		err := attr.g.GetFrom(m1)
+		if errors.Is(err, ErrAttributeNotFound) {
+			t.Fatalf("Unexpected 404: %s", err)
 		}
-		df, err := os.Open(filepath.Join(p, d.Name()))
+		if err != nil {
+			return
+		}
+
+		m2.WriteHeader()
+		if err = attr.g.AddTo(m2); err != nil {
+			// We allow decoding some text attributes
+			// when their length is too big, but
+			// not encoding.
+			if !IsAttrSizeOverflow(err) {
+				t.Fatal(err)
+			}
+			return
+		}
+
+		m3.WriteHeader()
+		v, err := m2.Get(attr.t)
 		if err != nil {
 			t.Fatal(err)
 		}
-		buf := make([]byte, 5000)
-		n, _ := df.Read(buf)
-		data = append(data, buf[:n])
-		df.Close()
-	}
-	return data
-}
+		m3.Add(attr.t, v)
 
-func TestFuzzMessage_Coverage(t *testing.T) {
-	for _, buf := range corpus(t, "stun-msg", "corpus") {
-		FuzzMessage(buf)
-	}
-}
-
-func TestFuzzMessage_Crashers(t *testing.T) {
-	for _, buf := range corpus(t, "stun-msg", "crashers") {
-		FuzzMessage(buf)
-	}
-}
-
-func TestFuzzType_Coverage(t *testing.T) {
-	for _, buf := range corpus(t, "stun-typ", "corpus") {
-		FuzzType(buf)
-	}
-}
-
-func TestFuzzType_Crashers(t *testing.T) {
-	for _, buf := range corpus(t, "stun-typ", "crashers") {
-		FuzzType(buf)
-	}
+		if !m2.Equal(m3) {
+			t.Fatalf("Not equal: %s != %s", m2, m3)
+		}
+	})
 }
 
 func TestAttrPick(t *testing.T) {
-	attributes := attrs{
+	attrs := attributes{
 		{new(XORMappedAddress), AttrXORMappedAddress},
 	}
+
 	for i := byte(0); i < 255; i++ {
-		attributes.pick(i)
+		attrs.pick(i)
 	}
 }
 
-func TestFuzzSetters_Crashers(t *testing.T) {
-	for _, buf := range corpus(t, "stun-setters", "crashers") {
-		FuzzSetters(buf)
-	}
+type attr interface {
+	Getter
+	Setter
 }
 
-func TestFuzzSetters_Coverage(t *testing.T) {
-	for _, buf := range corpus(t, "stun-setters", "corpus") {
-		FuzzSetters(buf)
-	}
+type attributes []struct {
+	g attr
+	t AttrType
+}
+
+func (a attributes) pick(v byte) struct {
+	g attr
+	t AttrType
+} {
+	idx := int(v) % len(a)
+	return a[idx]
 }
