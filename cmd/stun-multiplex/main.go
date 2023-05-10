6d6108d7c86128a37aa460adefe56b634cdac395
Steffen Vogel <post@steffenvogel.de>
2023-05-10 14:20:10 +0200

    Use log package instead of fmt for logging

--- cmd/stun-multiplex/main.go
+++ cmd/stun-multiplex/main.go
@@ -8,8 +8,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
 	"os/signal"
@@ -30,10 +30,10 @@ func keepAlive(c *stun.Client) {
 	for range t.C {
 		if err := c.Do(stun.MustBuild(stun.TransactionID, stun.BindingRequest), func(res stun.Event) {
 			if res.Error != nil {
-				panic(res.Error)
+				log.Panicf("Failed STUN transaction: %s", res.Error)
 			}
 		}); err != nil {
-			panic(err)
+			log.Panicf("Failed STUN transaction: %s", err)
 		}
 	}
 }
@@ -48,17 +48,18 @@ func demultiplex(conn *net.UDPConn, stunConn io.Writer, messages chan message) {
 	for {
 		n, raddr, err := conn.ReadFrom(buf)
 		if err != nil {
-			panic(err)
+			log.Panicf("Failed to read: %s", err)
 		}
+
 		// De-multiplexing incoming packets.
 		if stun.IsMessage(buf[:n]) {
 			// If buf looks like STUN message, send it to STUN client connection.
 			if _, err = stunConn.Write(buf[:n]); err != nil {
-				panic(err)
+				log.Panicf("Failed to write: %s", err)
 			}
 		} else {
 			// If not, it is application data.
-			fmt.Printf("demultiplex: [%s]: %s\n", raddr, buf[:n])
+			log.Printf("Demultiplex: [%s]: %s", raddr, buf[:n])
 			messages <- message{
 				text: string(buf[:n]),
 				addr: raddr,
@@ -73,10 +74,10 @@ func multiplex(conn *net.UDPConn, stunAddr net.Addr, stunConn io.Reader) {
 	for {
 		n, err := stunConn.Read(buf)
 		if err != nil {
-			panic(err)
+			log.Panicf("Failed to read: %s", err)
 		}
 		if _, err = conn.WriteTo(buf[:n], stunAddr); err != nil {
-			panic(err)
+			log.Panicf("Failed to write: %s", err)
 		}
 	}
 }
@@ -84,34 +85,45 @@ func multiplex(conn *net.UDPConn, stunAddr net.Addr, stunConn io.Reader) {
 var stunServer = flag.String("stun", "stun.l.google.com:19302", "STUN Server to use") //nolint:gochecknoglobals
 
 func main() {
+	isServer := flag.Arg(0) == ""
+
 	flag.Parse()
+
 	// Allocating local UDP socket that will be used both for STUN and
 	// our application data.
 	addr, err := net.ResolveUDPAddr("udp4", "0.0.0.0:0")
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to resolve: %s", err)
 	}
+
 	conn, err := net.ListenUDP("udp4", addr)
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to listen: %s", err)
 	}
-	// Resolving STUN Server address.
+
+	// Resolving STUN server address.
 	stunAddr, err := net.ResolveUDPAddr("udp4", *stunServer)
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to resolve '%s': %s", *stunServer, err)
 	}
-	fmt.Println("local addr:", conn.LocalAddr(), "stun server addr:", stunAddr)
+
+	log.Printf("Local address: %s", conn.LocalAddr())
+	log.Printf("STUN server address: %s", stunAddr)
+
 	stunL, stunR := net.Pipe()
+
 	c, err := stun.NewClient(stunR)
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to create client: %s", err)
 	}
+
 	// Starting multiplexing (writing back STUN messages) with de-multiplexing
 	// (passing STUN messages to STUN client and processing application
 	// data separately).
 	//
 	// stunL and stunR are virtual connections, see net.Pipe for reference.
 	messages := make(chan message)
+
 	go demultiplex(conn, stunL, messages)
 	go multiplex(conn, stunAddr, stunL)
 
@@ -124,17 +136,18 @@ func main() {
 	var gotAddr stun.XORMappedAddress
 	if err = c.Do(stun.MustBuild(stun.TransactionID, stun.BindingRequest), func(res stun.Event) {
 		if res.Error != nil {
-			panic(res.Error)
+			log.Panicf("Failed STUN transaction: %s", res.Error)
 		}
 		var xorAddr stun.XORMappedAddress
 		if getErr := xorAddr.GetFrom(res.Message); getErr != nil {
-			panic(getErr)
+			log.Panicf("Failed to get XOR-MAPPED-ADDRESS: %s", getErr)
 		}
 		copyAddr(&gotAddr, xorAddr)
 	}); err != nil {
-		panic(err)
+		log.Panicf("Failed STUN transaction: %s", err)
 	}
-	fmt.Println("public addr:", gotAddr)
+
+	log.Printf("Public address: %s", gotAddr)
 
 	// Keep-alive is needed to keep our NAT port allocated.
 	// Any ping-pong will work, but we are just making binding requests.
@@ -144,48 +157,56 @@ func main() {
 
 	notify := make(chan os.Signal, 1)
 	signal.Notify(notify, os.Interrupt, syscall.SIGTERM)
-	if flag.Arg(0) == "" {
-		fmt.Println("Acting as server. Use following command to connect:")
-		fmt.Println(os.Args[0], gotAddr)
+	if isServer {
+		log.Printf("Acting as server. Use following command to connect: %s %s", os.Args[0], gotAddr)
+
 		for {
 			select {
 			case m := <-messages:
 				if _, err = conn.WriteTo([]byte(m.text), m.addr); err != nil {
-					panic(err)
+					log.Panicf("Failed to write: %s", err)
 				}
 			case <-notify:
-				fmt.Println("\rStopping")
+				log.Println("Stopping")
 				return
 			}
 		}
 	} else {
 		peerAddr, err := net.ResolveUDPAddr("udp4", flag.Arg(0))
 		if err != nil {
-			panic(err)
+			log.Panicf("Failed to resolve '%s': %s", flag.Arg(0), err)
 		}
-		fmt.Println("Acting as client. Connecting to", peerAddr)
+
+		log.Printf("Acting as client. Connecting to %s", peerAddr)
+
 		msg := "Hello peer"
+
 		sendMsg := func() {
-			fmt.Println("Writing", peerAddr)
+			log.Printf("Writing to: %s", peerAddr)
 			if _, err = conn.WriteTo([]byte(msg), peerAddr); err != nil {
-				panic(err)
+				log.Panicf("Failed to write: %s", err)
 			}
 		}
+
 		sendMsg()
+
 		deadline := time.After(time.Second * 10)
+
 		for {
 			select {
 			case <-deadline:
-				fmt.Println("Failed to connect: deadline reached.")
-				os.Exit(2)
+				log.Fatal("Failed to connect: deadline reached.")
+
 			case <-time.After(time.Second):
 				// Retry.
 				sendMsg()
+
 			case m := <-messages:
-				fmt.Printf("Got response from %s: %s\n", m.addr, m.text)
+				log.Printf("Got response from %s: %s", m.addr, m.text)
 				return
+
 			case <-notify:
-				fmt.Println("\rStopping")
+				log.Print("Stopping")
 				return
 			}
 		}
