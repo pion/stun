2f130746f2a9844492b9b3d98e8a3d11d7e45a88
gongluck <gongluck93@gmail.com>
2024-07-25 11:54:18 +0800

    Call `flag.Parse()` first to enable normal reading of parameters

--- cmd/stun-multiplex/main.go
+++ cmd/stun-multiplex/main.go
@@ -85,9 +85,9 @@ func multiplex(conn *net.UDPConn, stunAddr net.Addr, stunConn io.Reader) {
 var stunServer = flag.String("stun", "stun.l.google.com:19302", "STUN Server to use") //nolint:gochecknoglobals
 
 func main() {
-	isServer := flag.Arg(0) == ""
-
 	flag.Parse()
+
+	isServer := flag.Arg(0) == ""
 
 	// Allocating local UDP socket that will be used both for STUN and
 	// our application data.
