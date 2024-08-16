d49a21ad0f4507fafb711211bb8bb1f2c966e207
Pion <59523206+pionbot@users.noreply.github.com>
2024-08-16 15:19:46 +0000

    Update CI configs to v0.11.15
    Update lint scripts and CI configs.

--- uri_test.go
+++ uri_test.go
@@ -85,7 +85,7 @@ func TestParseURL(t *testing.T) {
 			case errors.As(err, &urlError):
 				err = urlError.Err
 			case errors.As(err, &addrError):
-				err = fmt.Errorf(addrError.Err) //nolint:goerr113
+				err = fmt.Errorf(addrError.Err) //nolint:goerr113, govet
 			}
 			assert.EqualError(t, err, testCase.expectedErr.Error(), "testCase: %d %v", i, testCase)
 		}
