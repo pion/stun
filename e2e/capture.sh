76f3a9ffabac570cca57c006ae6e178b12c02caf
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:45:39 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- e2e/capture.sh
+++ e2e/capture.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+# SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 # SPDX-License-Identifier: MIT
 
 echo "net: $INTERFACE $SUBNET"
